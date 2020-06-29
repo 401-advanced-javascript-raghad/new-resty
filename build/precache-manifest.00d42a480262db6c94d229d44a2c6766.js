f435ca9ed837a7e4837596b705b4ce3b5c48b931
raghadalquran <raghadalquran1@gmail.com>
2020-06-29 22:42:08 +0300

    add README

---
+++ build/precache-manifest.00d42a480262db6c94d229d44a2c6766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9047ac896b7a8c95464fd3218ec1e15c",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "7bbce77cd5ca02320fca",
+    "url": "/resty/static/css/main.2a68faed.chunk.css"
+  },
+  {
+    "revision": "eb324abed117e1db57de",
+    "url": "/resty/static/js/2.59f6a094.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.59f6a094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbce77cd5ca02320fca",
+    "url": "/resty/static/js/main.2dfab461.chunk.js"
+  },
+  {
+    "revision": "c013a8649895b0139ced",
+    "url": "/resty/static/js/runtime-main.9749d002.js"
+  }
+]);
